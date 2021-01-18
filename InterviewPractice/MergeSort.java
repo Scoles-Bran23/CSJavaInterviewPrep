d0e68ba52b32e7e0d3055244bb0149157ecde5e3
Katie SB <katescoles-bran23@marlborough.org>
2021-01-17 19:54:47 -0800

    Update MergeSort.java

--- InterviewPractice/MergeSort.java
+++ InterviewPractice/MergeSort.java
@@ -1,3 +1,12 @@
+//Resources: https://www.youtube.com/watch?v=yv6svAfoYik
+/*
+How to code the merge sort algorithm in Java 8.
+Merge sort is a sorting algorithm that splits an array into halves until each
+half has a single element. It then merges the split arrays,
+returns the merge, and continues up the recursive ladder
+until the whole sorted array is returned.
+*/
+
 public class MergeSort {
 
 
@@ -5,6 +14,7 @@ public class MergeSort {
     for(int i : array){
       System.out.print(i + " ");
     }
+    System.out.println();
   }
 
   private static int[] mergeSort(int[] array){
@@ -16,7 +26,69 @@ public class MergeSort {
     int midpoint = array.length / 2;
 
     int[] left = new int[midpoint];
-    int[] right = new int[midpoint];
+    int[] right;
+    //in case the length of the array is uneven
+    if(array.length % 2 == 0){
+      right = new int[midpoint];
+    }
+    else{
+      right = new int[midpoint + 1];
+    }
+
+    //populate left and right arrays
+    for(int i = 0; i < midpoint; i++){
+      left[i] = array[i];
+    }
+    for(int j = 0; j < right.length; j++){
+      right[j] = array[midpoint + j];
+    }
+    int[] result = new int[array.length];
+    //recursive: calling itself inside function
+    left = mergeSort(left);
+    right = mergeSort(right);
+
+    result = merge(left, right);
+
+    return result;
+
+  }
+
+  private static int[] merge(int[] left, int[] right){
+    int[] result = new int[left.length + right.length];
+
+    int leftPointer, rightPointer, resultPointer;
+    //initialize them all to zero
+    leftPointer = rightPointer = resultPointer = 0;
+
+    while(leftPointer < left.length || rightPointer < right.length){
+      //checking if they both have elements in them
+      if(leftPointer < left.length && rightPointer < right.length){
+        if(left[leftPointer] < right[rightPointer]){
+          result[resultPointer++] = left[leftPointer++];
+        }
+        else {
+          result[resultPointer++] = right[rightPointer++];
+        }
+      }
+      //only elements in left array
+      else if(leftPointer < left.length){
+        result[resultPointer++] = left[leftPointer++];
+      }
+      //only elements in right array
+      else if(rightPointer < right.length){
+        result[resultPointer++] = right[rightPointer++];
+      }
+    }
+    return result;
+  }
+
+  public static void main(String args[]){
+    int[] array = {5, 4, 3, 2, 1};
+    System.out.println("initial array: ");
+    printArray(array);
+    array = mergeSort(array);
+    System.out.println("sorted array: ");
+    printArray(array);
   }
 
 }
