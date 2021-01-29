b505ec501b41ad5d5e820262f5fae24732989bc9
Katie SB <katescoles-bran23@marlborough.org>
2021-01-29 10:01:20 -0800

    a

--- InterviewPractice/BinarySearchFunction.java
+++ InterviewPractice/BinarySearchFunction.java
@@ -1,34 +1,42 @@
-public class BinarySearchFunction {
+class BinarySearchFunction {
+    // Returns index of x if it is present in arr[],
+    // else return -1
+    int binarySearch(int arr[], int x)
+    {
+        int l = 0, r = arr.length - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
 
-  public int binarySearchIterative(int arr[], int x){
-    int l = 0;
-    int r = arr.length - 1;
-    while (l <= r){
-      int m = 1 + (r - 1)/2;
+            // Check if x is present at mid
+            if (arr[m] == x)
+                return m;
 
-      if(arr[m] == x){
-        return m;
-      }
-      if(arr[m] > x){
-        l = m + 1;
-      }
-      else{
-        r = m - 1;
-      }
+            // If x greater, ignore left half
+            if (arr[m] < x)
+                l = m + 1;
+
+            // If x is smaller, ignore right half
+            else
+                r = m - 1;
+        }
+
+        // if we reach here, then element was
+        // not present
+        return -1;
     }
-    return -1;
-  }
-  public static void main(String args[]){
-    BinarySearchFunction ob = new BinarySearch;
-    int arr[] = {2, 3, 4, 10, 40};
-    int n = arr.length;
-    int x = 10;
-    int result = ob.binarySearch(arr, x);
-    if(result == -1){
-      System.out.println("nope");
+
+    // Driver method to test above
+    public static void main(String args[])
+    {
+        BinarySearchFunction ob = new BinarySearchFunction(); 
+        int arr[] = { 2, 3, 4, 10, 40 };
+        int n = arr.length;
+        int x = 10;
+        int result = ob.binarySearch(arr, x);
+        if (result == -1)
+            System.out.println("Element not present");
+        else
+            System.out.println("Element found at "
+                               + "index " + result);
     }
-    else{
-      System.out.println("element at " + result);
-    }
-  }
 }
